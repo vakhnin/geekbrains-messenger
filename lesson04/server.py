ac3f3ef685a3b0e31fd1dcfa850b18402317247d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-10-10 10:09:38 +0300

    Перенос функций сервера в файл utils

--- lesson04/server.py
+++ lesson04/server.py
@@ -19,47 +19,11 @@
 # * имеет параметры командной строки:
 #   -p <port> — TCP-порт для работы (по умолчанию использует 7777);
 #   -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
-import argparse
 import json
 import sys
 from socket import socket, SOCK_STREAM
 
-
-def create_parser():
-    parser_ = argparse.ArgumentParser()
-    parser_.add_argument('-a', default='')
-    parser_.add_argument('-p', type=int, default=7777)
-    return parser_
-
-
-def make_answer(code, message=None):
-    answer_ = {'response': code}
-    if not message:
-        return answer_
-    if 'error' in message.keys():
-        answer_['error'] = message['error']
-    elif 'alert' in message.keys():
-        answer_['alert'] = message['alert']
-    return answer_
-
-
-def parse_presence(jim_obj_):
-    if 'user' not in jim_obj_.keys():
-        return make_answer(400, {'error': 'Request has no "user"'})
-    elif type(jim_obj_['user']) != dict:
-        return make_answer(400, {'error': '"user" is not dict'})
-    elif 'account_name' not in jim_obj_['user'].keys():
-        return make_answer(400, {'error': '"user" has no "account_name"'})
-    elif not jim_obj_['user']['account_name']:
-        return make_answer(400, {'error': '"account_name" is empty'})
-    else:
-        print(f'User {jim_obj_["user"]["account_name"]} is presence')
-        if 'status' in jim_obj_['user'].keys() \
-                and jim_obj_['user']['status']:
-            print(f'Status user{jim_obj_["user"]["account_name"]} is "' +
-                  jim_obj_['user']['status'] + '"')
-        return make_answer(200)
-
+from common.utils import create_parser, make_answer, parse_presence
 
 parser = create_parser()
 namespace = parser.parse_args(sys.argv[1:])
