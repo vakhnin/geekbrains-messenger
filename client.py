a2589108ca90f994c745717b0c715d55e962c6ed
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-10-11 10:26:27 +0300

    Рефактор кода клиента

--- client.py
+++ client.py
@@ -1,27 +1,21 @@
-import sys
-from socket import socket, SOCK_STREAM
+from common.utils import parse_answer, make_presence_message, \
+    send_message_take_answer, make_sent_socket
 
-from common.utils import parse_answer, make_presence_message, send_message_take_answer
-from common.vars import DEFAULT_IP_ADDRESS, DEFAULT_PORT
 
-try:
-    addr, port = DEFAULT_IP_ADDRESS, DEFAULT_PORT
-    if len(sys.argv) > 1:
-        addr = sys.argv[1]
-    if len(sys.argv) > 2:
-        port = int(sys.argv[2])
-    print('Соединение с сервером установлено')
+def main():
+    try:
+        sock = make_sent_socket()
 
-    sock = socket(type=SOCK_STREAM)
-    sock.connect((addr, port))
+        message = make_presence_message('C0deMaver1ck', 'Yep, I am here!')
+        answer = send_message_take_answer(sock, message)
+        parse_answer(answer)
 
-    message = make_presence_message('C0deMaver1ck', 'Yep, I am here!')
-    answer = send_message_take_answer(sock, message)
-    parse_answer(answer)
-except ConnectionRefusedError:
-    err_msg = 'Подключение не установлено, т.к. конечный компьютер ' + \
-              'отверг запрос на подключение'
-    print(err_msg)
-finally:
-    sock.close()
-    print('Соединение с сервером закрыто')
+        sock.close()
+    except ConnectionRefusedError:
+        err_msg = 'Подключение не установлено, т.к. конечный компьютер ' + \
+                  'отверг запрос на подключение'
+        print(err_msg)
+
+
+if __name__ == '__main__':
+    main()
