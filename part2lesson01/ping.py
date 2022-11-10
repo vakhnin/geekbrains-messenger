5a6582cdd3ff0c9d8f8c0309610c8907b9ac1ae9
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-11-10 15:32:18 +0300

    Функция host_range_ping()

--- part2lesson01/ping.py
+++ part2lesson01/ping.py
@@ -1,3 +1,8 @@
+import platform
+from ipaddress import ip_address, ip_network
+from pprint import pprint
+from subprocess import Popen, DEVNULL
+
 # Написать функцию host_ping(), в которой с помощью утилиты ping будет проверяться
 # доступность сетевых узлов.
 # Аргументом функции является список, в котором каждый сетевой узел
@@ -5,11 +10,6 @@
 # В функции необходимо перебирать ip-адреса и проверять их доступность с выводом с
 # оответствующего сообщения («Узел доступен», «Узел недоступен»).
 # При этом ip-адрес сетевого узла должен создаваться с помощью функции ip_address().
-import platform
-from ipaddress import ip_address
-from pprint import pprint
-from subprocess import Popen, DEVNULL
-
 REACHABLEMESSAGE = 'Узел доступен'
 UNREACHABLEMESSAGE = 'Узел не доступен'
 
@@ -36,8 +36,47 @@ def host_ping(ip_list):
 
     return ping_list
 
+# Написать функцию host_range_ping() для перебора ip-адресов из заданного диапазона.
+# Меняться должен только последний октет каждого адреса.
+# По результатам проверки должно выводиться соответствующее сообщение.
+def host_range_ping(start_ip, count_ip):
+    if count_ip < 1 or not type(count_ip) == int:
+        print(f'Количество адресов должно быть целым числом 1 или больше')
+        return False
+
+    try:
+        ip = ip_address(start_ip)
+    except ValueError:
+        print(f'{start_ip} не является валидным ip')
+        return False
+
+    last_octet = int(ip) % 256
+    if last_octet + count_ip > 255:
+        print(f'От ip адреса {start_ip} в последний октет не помещается'
+              f' {count_ip} адреса, только {255 - last_octet}')
+        return False
+
+    ip_list = []
+    for i in range(count_ip):
+        ip_list.append(ip + i)
+    return host_ping(ip_list)
+
 
 print('==================== Проверка функции host_ping() ======================================================')
-list_for_ping = ['google.com', 'a', '8.8.8.8']
-result = host_ping(list_for_ping)
-pprint(result)
+# list_for_ping = ['google.com', 'a', '8.8.8.8']
+# result = host_ping(list_for_ping)
+# pprint(result)
+
+print('==================== Проверка функции host_range_ping() ================================================')
+while True:
+    ip = input('Введите начальный IP или q для выхода: ')
+    if ip == 'q':
+        break
+    count = input('Введите количестов IP для тестирования или q для выхода: ')
+    if count == 'q':
+        break
+    count = int(count)
+    result = host_range_ping(ip, count)
+    if result:
+        pprint(result)
+        break
