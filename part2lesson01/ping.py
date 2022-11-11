d207be245605d151cbaaccb47bb0eda3ea15b242
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-11-11 11:12:17 +0300

    Функция host_range_ping_tab()

--- part2lesson01/ping.py
+++ part2lesson01/ping.py
@@ -1,7 +1,10 @@
 import platform
 from ipaddress import ip_address, ip_network
+from itertools import zip_longest
 from pprint import pprint
 from subprocess import Popen, DEVNULL
+
+from tabulate import tabulate
 
 # Написать функцию host_ping(), в которой с помощью утилиты ping будет проверяться
 # доступность сетевых узлов.
@@ -26,15 +29,16 @@ def host_ping(ip_list):
         command = ["ping", param, "2", str(ip)]
         process = Popen(command, stdout=DEVNULL, stderr=DEVNULL)
         ping_processes_list.append({'ip': ip,
-                                    'reachable': process})
+                                    'process': process})
 
     ping_list = []
     for i in range(len(ping_processes_list)):
-        ping_result = ping_processes_list[i]['reachable'].wait()
+        ping_result = ping_processes_list[i]['process'].wait()
         ping_list.append({'ip': ping_processes_list[i]['ip'],
                           'reachable': UNREACHABLEMESSAGE if ping_result else REACHABLEMESSAGE})
 
     return ping_list
+
 
 # Написать функцию host_range_ping() для перебора ip-адресов из заданного диапазона.
 # Меняться должен только последний октет каждого адреса.
@@ -62,10 +66,32 @@ def host_range_ping(start_ip, count_ip):
     return host_ping(ip_list)
 
 
+# Написать функцию host_range_ping_tab(), возможности которой основаны на функции из примера 2.
+# Но в данном случае результат должен быть итоговым по всем ip-адресам, представленным
+# в табличном формате (использовать модуль tabulate). Таблица должна состоять из двух колонок
+def host_range_ping_tab(start_ip, count_ip):
+    ip_list = host_range_ping(start_ip, count_ip)
+    if not ip_list:
+        print('Список IP пуст')
+        return False
+
+    reachable_ip_list = []
+    unreachable_ip_list = []
+    for item in ip_list:
+        if item['reachable'] == REACHABLEMESSAGE:
+            reachable_ip_list.append(str(item['ip']))
+        else:
+            unreachable_ip_list.append(str(item['ip']))
+
+    columns = ('Reachable', 'Unreachable')
+    result_table = list(zip_longest(reachable_ip_list, unreachable_ip_list, fillvalue=''))
+    return tabulate(result_table, headers=columns, tablefmt='grid')
+
+
 print('==================== Проверка функции host_ping() ======================================================')
-# list_for_ping = ['google.com', 'a', '8.8.8.8']
-# result = host_ping(list_for_ping)
-# pprint(result)
+list_for_ping = ['google.com', 'a', '8.8.8.8']
+result = host_ping(list_for_ping)
+pprint(result)
 
 print('==================== Проверка функции host_range_ping() ================================================')
 while True:
@@ -79,4 +105,18 @@ while True:
     result = host_range_ping(ip, count)
     if result:
         pprint(result)
+        break
+
+print('==================== Проверка функции host_range_ping_tab() ============================================')
+while True:
+    ip = input('Введите начальный IP или q для выхода: ')
+    if ip == 'q':
+        break
+    count = input('Введите количестов IP для тестирования или q для выхода: ')
+    if count == 'q':
+        break
+    count = int(count)
+    result = host_range_ping_tab(ip, count)
+    if result:
+        print(result)
         break
