9bf6b90fa73a009d36be7ba836109c11bfd524de
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-11-21 16:24:44 +0300

    Вывод списка пользователей в GUI серевера

--- server_gui/server_gui_utils.py
+++ server_gui/server_gui_utils.py
@@ -1,7 +1,10 @@
 import os
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+from storage.models import engine
+from storage.server import Storage
 
 cur_path = os.path.abspath(__file__)
 cur_dir, _ = os.path.split(cur_path)
@@ -12,3 +15,14 @@ class ServerGUIWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         uic.loadUi(cur_dir + 'serverUI.ui', self)
+        self.storage = Storage(engine)
+
+    def show_users_list(self):
+        users_table = self.usersTableWidget
+        users_list = self.storage.user_list()
+
+        users_table.setRowCount(len(users_list))
+        for row, user in enumerate(users_list):
+            users_table.setItem(row, 0, QTableWidgetItem(str(user.id)))
+            users_table.setItem(row, 1, QTableWidgetItem(str(user.login)))
+            users_table.setItem(row, 2, QTableWidgetItem(str(user.info)))
--- start_server_gui.py
+++ start_server_gui.py
@@ -8,5 +8,6 @@ from server_gui.server_gui_utils import ServerGUIWindow
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = ServerGUIWindow()
+    window.show_users_list()
     window.show()
     sys.exit(app.exec_())
